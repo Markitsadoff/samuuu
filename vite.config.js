4e261b678f2494a7a56ea67a2d348dc6739b9520
Markitsadoff <109414828+Markitsadoff@users.noreply.github.com>
2025-11-02 19:17:02 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// 👇 Добавляем ключ "type": "module" в package.json, чтобы Vercel понимал ESM
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 }
-})
+  build: {
+    outDir: "dist",
+  },
+});
